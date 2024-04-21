232f7e00d6da643a948bfdbcf50d8d29a947cdea
Juni May <juni_may@outlook.com>
2024-04-21 20:00:49 +0800

    feat(riscv): addi op

--- orzir/src/dialects/riscv.rs
+++ orzir/src/dialects/riscv.rs
@@ -1,1 +1,4 @@
+pub mod basic;
 pub mod regs;
+pub mod rv_f;
+pub mod rv_m;
---
+++ orzir/src/dialects/riscv/basic.rs
@@ -0,0 +1,57 @@
+use std::fmt::Write;
+
+use orzir_core::{
+    apint::ApInt, verification_error, ArenaPtr, Context, Dialect, Op, OpMetadata, Parse,
+    RunVerifiers, Value, Verify,
+};
+use orzir_macros::{ControlFlow, DataFlow, Op, Parse, Print, RegionInterface};
+use thiserror::Error;
+
+use super::regs::IReg;
+use crate::verifiers::*;
+
+#[derive(Debug, Error)]
+#[error("expected an immediate with width at most {0}, but got {1}")]
+pub struct ImmOutOfRangeErr(pub usize, pub usize);
+
+#[derive(Op, DataFlow, RegionInterface, ControlFlow, Parse, Print)]
+#[mnemonic = "rv.addi"]
+#[verifiers(
+    NumResults<1>, NumOperands<1>, NumRegions<0>,
+    SameResultTys, SameOperandTys, SameOperandAndResultTys,
+    IntegerLikeOperands,
+    OperandTyIs<IReg>, ResultTyIs<IReg>
+)]
+#[format(pattern = "{lhs} , {imm}", kind = "op", num_results = 1)]
+pub struct AddiOp {
+    #[metadata]
+    metadata: OpMetadata,
+    /// The result of the operation.
+    #[result(0)]
+    result: ArenaPtr<Value>,
+    /// The left-hand side operand.
+    #[operand(0)]
+    lhs: ArenaPtr<Value>,
+    /// The right-hand side immediate.
+    imm: ApInt,
+}
+
+impl Verify for AddiOp {
+    fn verify(&self, ctx: &Context) -> orzir_core::VerificationResult<()> {
+        self.run_verifiers(ctx)?;
+
+        // verify the width of the immediate
+        if self.imm.width() > 12 {
+            return verification_error!(ImmOutOfRangeErr(12, self.imm.width())).into();
+        }
+
+        Ok(())
+    }
+}
+
+pub fn register(ctx: &mut Context) {
+    let dialect = Dialect::new("rv".into());
+    ctx.dialects.insert("rv".into(), dialect);
+
+    AddiOp::register(ctx, AddiOp::parse);
+}
