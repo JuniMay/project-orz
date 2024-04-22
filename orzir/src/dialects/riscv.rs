fbe0b9fe973911694843c55b6fc632f046dfa657
Juni May <juni_may@outlook.com>
2024-04-22 21:19:29 +0800

    feat(riscv): add rv_m dialect

--- orzir/src/dialects/riscv.rs
+++ orzir/src/dialects/riscv.rs
@@ -1,4 +1,23 @@
+use orzir_core::Context;
+
 pub mod basic;
 pub mod regs;
 pub mod rv_f;
 pub mod rv_m;
+
+/// Register the riscv dialects according to the ISA extension name.
+pub fn register_riscv_dialects(ctx: &mut Context, spec: &str) {
+    let spec = spec.to_lowercase();
+    let spec = spec.replace('g', "imafd_zicsr_zifencei");
+
+    basic::register(ctx);
+
+    if spec.contains('m') {
+        rv_m::register(ctx);
+    }
+
+    if spec.contains('f') {
+        rv_f::register(ctx);
+        // TODO: RVF implies Zicsr extension.
+    }
+}
--- orzir/src/dialects/riscv/rv_f.rs
+++ orzir/src/dialects/riscv/rv_f.rs
@@ -1,1 +1,6 @@
+use orzir_core::{Context, Dialect};
 
+pub fn register(ctx: &mut Context) {
+    ctx.dialects
+        .insert("rv_f".into(), Dialect::new("rv_f".into()));
+}
